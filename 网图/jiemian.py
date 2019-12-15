d0da8ba67fbf65664bf4b7c47388902b71b0fb37
luojunjie <905536460@qq.com.com>
2019-12-15 13:57:02 +0800

    1

--- 网图/jiemian.py
+++ 网图/jiemian.py
@@ -10,7 +10,8 @@ dy = []     #每层经销商
 btlist = [] #表头字典
 xslie= [] #要显示的列
 xslist = [] #要显示的字段
-a = False
+xssj = [] #要显示的字段序号
+cf = False
 
 def thread_it(func, *args):
     '''将函数打包进线程'''
@@ -23,8 +24,9 @@ def thread_it(func, *args):
     # 阻塞--卡死界面！
     # t.join()
 
+#写入表头信息框架frame4数据
 def func():
-    filePath = var2.get()
+    filePath = var_path.get()
     # 打开excel
     date = pd.read_excel(filePath, header=None)
     f = []
@@ -36,17 +38,21 @@ def func():
         for i in range(0, btL):
             li = bt[i].strip()
             btlist.append(li)
+    #写入frame4中
     for a in range(0,len(btlist)):
         v.append(tk.IntVar())
         b = tk.Checkbutton(frame4, text=btlist[a], variable=v[-1])
         b.grid(row=0, column=a)
 
 #D:\咨委推荐名单调取数据2_20191127（20191210）(1).xlsx 0038501
+#C:\Users\jjfly\Desktop\咨委推荐名单调取数据2_20191127（20191210）.xlsx
 
 def func1():
-    global date,hs,dot,card1
-    card1 = var1.get()
-    filePath = var2.get()
+    global date,hs,dot,card1,hylie,tjrlie
+    card1 = var_card.get()
+    filePath = var_path.get()
+    hylie = int(var_hylie.get())
+    tjrlie = int(var_trjlie.get())
     for a in range(0,len(btlist)):
         if v[a].get() ==1:
             xslie.append(a)
@@ -61,46 +67,61 @@ def func1():
     xh(dy)
 
 def xh(zz):
-    global js
+    global js,cf
+    if cf == False:
+        for xx in range(0,len(btlist)):
+            if v[xx].get() == True:
+                xssj.append(xx)
+        cf = True
     f = []
     #循环整个文档行
     for i in range(1, hs):
-        zw = zh(date.loc[[i], [1]]) #执委
-        hy = zh(date.loc[[i], [2]])  # 会员编号
-        name = zh(date.loc[[i], [3]])  # 姓名
-        age = zh(date.loc[[i], [4]])  # 年龄
-        sex = zh(date.loc[[i], [5]])  # 性别
-        leg = zh(date.loc[[i], [6]]) #月均2.8万业绩leg数
-        qg = zh(date.loc[[i], [7]]) #19年业绩（切割）
-        bqg = zh(date.loc[[i], [8]]) #19年业绩
-        tjr = zh(date.loc[[i], [9]])    #紧急联系人卡号
+        mhz = []  # 存储每行显示值列表
+        str1 = '' #存储显示内容
+        for xx in range(0, len(xssj)):
+            b =  xssj[xx]
+            hy = zh(date.loc[[i], [hylie-1]])
+            tjr = zh(date.loc[[i], [tjrlie-1]])
+            mhz.append(zh(date.loc[[i], [b]]))
 
         #取第一层
-        if hy == card1:
-            A = zw + ' ' + hy + ' ' + name  + ' ' + str(age) + ' ' + sex  + '月均2.8万业绩leg数:' + str(leg) + '\n' + '19年业绩(切割):' + str('%.1f' % (qg / 10000)) + '万\n' + '19年业绩:' + str('%.1f' % (bqg / 10000)) + '万\n'
-            filename = zw
+        if  hy == card1:
+            for xx in range(0, len(xssj)):
+                b = xssj[xx]
+                str1.join(str(btlist[b]) + ':' +str(mhz[xx]) + '\n')
+                print(btlist[b],mhz[xx])
+            filename = card1
         # 查询每层符合条件的人
         if tjr in zz:
             #网图每个框显示的内容
-            B = zw + '-' + hy + ' ' + name + '\n' + '年龄:' + str(age) + ' 性别:' + sex + '\n' + '月均2.8万业绩leg数:' + str(
-                leg) + '\n' + '19年业绩(切割):' + str('%.1f' % (qg / 10000)) + '万\n' + '19年业绩:' + str(
-                '%.1f' % (bqg / 10000)) + '万\n'
+            for xx in range(0, len(xssj)):
+                b = xssj[xx]
+                str1.join(str(btlist[b]) + ':' + str(mhz[xx]) + '\n')
             f.append(hy)
             #绘制整个网图
             if True:
                 if js==1:
+                        dot.node(tjr, str1, fontname="SimHei", color='LightSkyBlue')
+                dot.node(tjr, fontname="SimHei")
+                dot.node(hy, str1, fontname="SimHei",color ='Pink')
+                dot.edge(tjr, hy)
+
+            '''
+            if True:
+                if js==1:
                     if sex =="男":
-                        dot.node(tjr, A, fontname="SimHei", color='LightSkyBlue')
+                        dot.node(tjr, str1, fontname="SimHei", color='LightSkyBlue')
                     else:
-                        dot.node(tjr, A, fontname="SimHei", color='Pink')
+                        dot.node(tjr, str1, fontname="SimHei", color='Pink')
                 if sex =="男":
                     dot.node(tjr, fontname="SimHei")
-                    dot.node(hy, B, fontname="SimHei",color ='LightSkyBlue')
+                    dot.node(hy, str1, fontname="SimHei",color ='LightSkyBlue')
                     dot.edge(tjr, hy)
                 else:
                     dot.node(tjr, fontname="SimHei")
-                    dot.node(hy, B, fontname="SimHei",color ='Pink')
+                    dot.node(hy, str1, fontname="SimHei",color ='Pink')
                     dot.edge(tjr, hy)
+            '''
     #判断
     if f != []:
         js = js + 1
@@ -108,8 +129,8 @@ def xh(zz):
     else:
         path = 'd://'+ filename +'.gv'
         dot.render(path, view=False)
-        label_savePath = tk.Label(frame1, text=path, font=('黑体', 8), bg='red')
-        label_savePath.pack(side='right')
+        Entry_savePath= tk.Entry(frame3, show=None,state ='readonly',textvariable=path, font=('黑体', 8), width=20)
+        Entry_savePath.pack(side=LEFT, fill=Y, expand=YES)
 
 
 #值转为列表
@@ -128,36 +149,32 @@ if __name__ == '__main__':
     frame = tk.LabelFrame(top)
     frame.pack()
 
-    # 创建框架frame7
-    frame7 = tk.Frame(frame)
-    frame7.pack()
 
-    # 创建第二层框架frame2
-    frame2 = tk.Frame(frame7)
-    frame2.pack(side='left')
-    label2 = tk.Label(frame2, text='卡号:', font=('黑体', 10))
-    var1 = tk.StringVar()
-    card = tk.Entry(frame2,show=None,textvariable=var1,font=('黑体', 8),width = 10)
-    label2.pack(side='left')
-    card.pack(side='right')
+    # 创建基本信息框架frame3
+    frame3 = tk.Frame(frame)
+    frame3.pack()
+    label_path = tk.Label(frame3, text='文档位置:', font=('黑体', 10)).pack(side=LEFT, fill=Y, expand=YES)
+    var_path = tk.StringVar()
+    filePath = tk.Entry(frame3,show=None,textvariable=var_path,font=('黑体', 8),width = 30).pack(side=LEFT, fill=Y, expand=YES)
+    label_card = tk.Label(frame3, text='卡号:', font=('黑体', 10)).pack(side=LEFT, fill=Y, expand=YES)
+    var_card = tk.StringVar()
+    hylie = tk.Entry(frame3,show=None,textvariable=var_card,font=('黑体', 8),width = 10).pack(side=LEFT, fill=Y, expand=YES)
+    label_hylie = tk.Label(frame3, text='卡号所属列:', font=('黑体', 10)).pack(side=LEFT, fill=Y, expand=YES)
+    var_hylie = tk.StringVar()
+    card = tk.Entry(frame3,show=None,textvariable=var_hylie,font=('黑体', 8),width = 5).pack(side=LEFT, fill=Y, expand=YES)
+    label_trjlie = tk.Label(frame3, text='推荐人所属列:', font=('黑体', 10)).pack(side=LEFT, fill=Y, expand=YES)
+    var_trjlie = tk.StringVar()
+    tjrlie = tk.Entry(frame3,show=None,textvariable=var_trjlie,font=('黑体', 8),width = 5).pack(side=LEFT, fill=Y, expand=YES)
 
-    # 创建第三层框架frame3
-    frame3 = tk.Frame(frame7)
-    frame3.pack(side='right')
-    label3 = tk.Label(frame3, text='文档位置:', font=('黑体', 10))
-    var2 = tk.StringVar()
-    filePath = tk.Entry(frame3,show=None,textvariable=var2,font=('黑体', 8),width = 30)
-    label3.pack(side='left')
-    filePath.pack(side='right')
 
-    button1 = tk.Button(frame, text='点击',command = lambda :thread_it(func))
+    button1 = tk.Button(frame, text='获取表头信息',command = lambda :thread_it(func))
     button1.pack()
 
-    # 创建第四层框架frame4
+    # 创建表头信息框架frame4
     frame4 = tk.LabelFrame(frame,text = '表头信息：',height= '50',width='700')
     frame4.pack()
     v = []
-    button2 = tk.Button(frame, text='点击',command = lambda :thread_it(func1))
+    button2 = tk.Button(frame, text='生成图',command = lambda :thread_it(func1))
     button2.pack()
 
     #创建进度条框架frame5
@@ -167,11 +184,10 @@ if __name__ == '__main__':
     canvas = tk.Canvas(frame5, width=465, height=22, bg="white")
     canvas.pack(side='right')
 
-    #创建第一层框架frame1
+    #创建文件储存位置框架frame1
     frame1 = tk.Frame(frame)
     frame1.pack()
-    label1 = tk.Label(frame1,text='文件储存位置:',font=('黑体',10))
-    label1.pack(side='left')
+    label1 = tk.Label(frame1,text='文件储存位置:',font=('黑体',10)).pack(side=LEFT, fill=Y,expand=YES)
 
 
     top.mainloop()
